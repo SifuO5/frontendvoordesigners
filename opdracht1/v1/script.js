e1a48b9f81ff0eab7452720d013c5fea84fbe15a
SifuO5 <39334903+SifuO5@users.noreply.github.com>
2019-05-07 10:16:35 +0200

    Add files via upload

---
+++ opdracht1/v1/script.js
@@ -0,0 +1,29 @@
+/*
+var heartIcon = document.querySelector('.heart-icon');
+
+heartIcon.addEventListener('click', function () {
+    console.log('joehoee');
+    heartIcon.classList.toggle("test");
+});
+*/
+
+
+var heartIconSVG = document.querySelector('.heartIconSVG');
+
+heartIconSVG.addEventListener('click', function () {
+    console.log('joehoeeSVG');
+    heartIconSVG.classList.toggle("red");
+});
+
+/*
+
+for (var button of likeButton) {
+  button.addEventListener("click", function() {
+    this.classList.tog
+    gle("active");
+    countLike();
+
+  });
+
+}
+*/
